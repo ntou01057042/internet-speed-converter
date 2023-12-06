6a031b6519c3efbf5d9323c1665b24cec106819d
ntou01057042 <01057042@email.ntou.edu.tw>
2023-12-06 21:23:16 +0800

    Add converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+import UnitControl from './components/UnitControl';
+import CardFooter from './components/CardFooter';
+import UnitConverter from './components/UnitConverter';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, far);
+
 function App() {
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleInputChange = (e) => setInputValue(e.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card-header">Network Speed Converter</div>
+      <div className="card-body">
+        <UnitControl />
+        <UnitConverter
+          handleInputChange={handleInputChange}
+          inputValue={inputValue}
+        />
+      </div>
+      <CardFooter inputValue={inputValue} />
     </div>
   );
 }
---
+++ src/components/UnitConverter.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const UnitConverter = ({ handleInputChange, inputValue }) => (
+  <div className="converter">
+    <div className="flex-1">
+      <div className="converter-title">Set</div>
+      <input
+        type="number"
+        onChange={handleInputChange}
+        className="input-number"
+        min="0"
+        value="0"
+      />
+    </div>
+    <span className="angle-icon fa-2x" style={{ marginTop: 30 }}>
+      <FontAwesomeIcon icon={['fas', 'angle-right']} />
+    </span>
+    <div className="text-right flex-1">
+      <div className="converter-title">Show</div>
+      <input
+        type="text"
+        className="input-number text-right"
+        disabled
+        value={inputValue / 8}
+      />
+    </div>
+  </div>
+);
+
+export default UnitConverter;
